089d7234e299a008cba9678dded724f3bf11eaa9
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-08-12 14:52:26 +0530

    Created availablecourse page and added javascript

---
+++ Frontend/myscript.js
@@ -0,0 +1,44 @@
+function showCourses() {
+    fetch('http://localhost:8080/courses')
+        .then((response) => response.json())
+        .then((courses) => {
+            const dataTable = document.getElementById('coursetable');
+                       
+            courses.forEach((course) => {
+                const row = `
+                    <tr>
+                        <td>${course.courseId}</td>
+                        <td>${course.courseName}</td>
+                        <td>${course.trainer}</td>
+                        <td>${course.durationInWeeks}</td>
+                    </tr>
+                `;
+                dataTable.innerHTML += row;
+            });
+        })
+        .catch((error) => {
+            console.error('Error fetching courses:', error);
+        });
+}
+
+function showEnrolledStudents() {
+    fetch('http://localhost:8080/courses/enrolled')
+        .then((response) => response.json())
+        .then((students) => {
+            const dataTable = document.getElementById('enrolledtable');
+                       
+            students.forEach((student) => {
+                const row = `
+                    <tr>
+                        <td>${student.name}</td>
+                        <td>${student.emailId}</td>
+                        <td>${student.courseName}</td>
+                    </tr>
+                `;
+                dataTable.innerHTML += row;
+            });
+        })
+        .catch((error) => {
+            console.error('Error fetching courses:', error);
+        });
+}
